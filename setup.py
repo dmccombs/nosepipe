8045a1c1d2d7dbe62e96bd127da9a22dbd1f2437
Dan McCombs <dmccombs@dyn.com>
2016-02-19 09:26:38 -0500

    Bump version to 0.8 and add release notes.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="nosepipe",
-    version="0.7",
+    version="0.8",
     download_url = "http://pypi.python.org/pypi/nosepipe/",
 
     description = "Plugin for the nose testing framework for running tests in "
